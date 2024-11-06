6ea3447e96541d2b8f37fb32f1a3316a0be85de5
Quilliou <erwan.quilliou1@etu.univ-lorraine.fr>
2024-11-06 20:42:41 +0100

    dernières modifs

--- deefy/src/classes/action/AddTrackAction.php
+++ deefy/src/classes/action/AddTrackAction.php
@@ -3,24 +3,33 @@
 namespace iutnc\deefy\action;
 
 use iutnc\deefy\audio\tracks as tracks;
-
+use iutnc\deefy\auth as auth;
+use iutnc\deefy\exception as exception;
 //Classe pour gerer l'ajout d'une Track
 class AddTrackAction extends Action
 {
     public function execute(): string
     {
         if($this->http_method  === 'GET'){
-            $html = <<<END
-            <form method = "post" action = "?action=add-track">
-                <label>Nom de la piste : <input type="text" name="nom" placeholder="nom"></label></br>
-                <label>chemin du fichier : <input type="text" name="chemin" placeholder="chemin fichier"></label></br>
-                <label>Nom de l'artiste : <input type="text" name="artiste" placeholder="artiste"></label></br>
-                <label>Nom de l'album : <input type="text" name="album" placeholder="album"></label></br>
-                <label>Nombre de pistes : <input type="text" name="nbPistes" placeholder="nbPistes"></label></br>
-                <label>Durée : <input type="text" name="durée" placeholder="durée"></label>
-                <button type="submit">Créer</button>
-            </form>
-            END;
+            $connect = true;
+            try{
+                auth\AuthnProvider::getSignInUser();
+            }catch(exception\AuthnException $e){
+                $html = $e->getMEssage(); $connect = false;
+            };
+            if($connect){
+                $html = <<<END
+                <form method = "post" action = "?action=add-track">
+                    <label>Nom de la piste : <input type="text" name="nom" placeholder="nom"></label></br>
+                    <label>chemin du fichier : <input type="text" name="chemin" placeholder="chemin fichier"></label></br>
+                    <label>Nom de l'artiste : <input type="text" name="artiste" placeholder="artiste"></label></br>
+                    <label>Nom de l'album : <input type="text" name="album" placeholder="album"></label></br>
+                    <label>Nombre de pistes : <input type="text" name="nbPistes" placeholder="nbPistes"></label></br>
+                    <label>Durée : <input type="text" name="durée" placeholder="durée"></label>
+                    <button type="submit">Créer</button>
+                </form>
+                END;
+            }
 
         }else{
             $nom = $_POST['nom'];
