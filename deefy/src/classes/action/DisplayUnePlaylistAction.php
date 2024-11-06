1ca1ed8ba141ff4c00cd30a795a23d69041009b1
Quilliou <erwan.quilliou1@etu.univ-lorraine.fr>
2024-11-06 10:52:40 +0100

    fonctionnalité affichage une playlist ok

--- deefy/src/classes/action/DisplayUnePlaylistAction.php
+++ deefy/src/classes/action/DisplayUnePlaylistAction.php
@@ -6,7 +6,7 @@ use iutnc\deefy\render as render;
 use iutnc\deefy\audio\lists\Playlist;
 
 //Classe pour gerer l'affichage d'une playlist
-class DisplayPlaylistAction extends Action
+class DisplayUnePlaylistAction extends Action
 {
 
     public function execute(): string
@@ -14,7 +14,7 @@ class DisplayPlaylistAction extends Action
         if($this->http_method  === 'GET'){
             $html = <<<END
             <form method="post" action="?action=une-playlist">
-                <select name="nomTrack">
+                <select name="idPlaylist">
             END;
             $repo = \iutnc\deefy\repository\DeefyRepository::getInstance();
             $array = $repo->findAllPlaylists();
@@ -26,9 +26,18 @@ class DisplayPlaylistAction extends Action
 
             $html .= <<<END
                 </select>
+                <button type="submit">Afficher</button>
+                </form>
             END;
         }else{
-
+            $repo = \iutnc\deefy\repository\DeefyRepository::getInstance();
+            $pl = $repo->findPlaylistById($_POST['idPlaylist']);
+            $html = "<div> voici votre playlist : </div>";
+            $html.="</br></br>";
+            $renderer = new render\AudioListRenderer($pl);
+            $html.= $renderer->render(1);
+            $html.="</br></br>";
         }
+        return $html;
     }
 }
